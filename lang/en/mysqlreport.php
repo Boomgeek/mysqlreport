7f19261246f69145de518982e04a3f0e683ba6b4
Supanut Dokmaithong <boomhack320@gmail.com>
2015-01-22 15:06:30 +0700

    เริ่มสร้างโมดูล mysqlreport ยังไม่ได้สร้างฐานข้อมูลที่ใช้กับโมดูล

---
+++ lang/en/mysqlreport.php
@@ -0,0 +1,39 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * English strings for mysqlreport
+ *
+ * You can have a rather longer description of the file as well,
+ * if you like, and it can span multiple lines.
+ *
+ * @package    mod_mysqlreport
+ * @copyright  2015 Your Name
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['modulename'] = 'mysqlreport';
+$string['modulenameplural'] = 'mysqlreports';
+$string['modulename_help'] = 'Use the mysqlreport module for... | The mysqlreport module allows...';
+$string['mysqlreportfieldset'] = 'Custom example fieldset';
+$string['mysqlreportname'] = 'mysqlreport name';
+$string['mysqlreportname_help'] = 'This is the content of the help tooltip associated with the mysqlreportname field. Markdown syntax is supported.';
+$string['mysqlreport'] = 'mysqlreport';
+$string['pluginadministration'] = 'mysqlreport administration';
+$string['pluginname'] = 'mysqlreport';
