0f6ba394a5e712b2f17bcc3fe09f9385bd734be0
Supanut Dokmaithong <boomhack320@gmail.com>
2015-01-28 21:29:42 +0700

    เพิ่มส่วนตรวจสอบการล็อกอิน

--- source/dashboard.php
+++ source/dashboard.php
@@ -1,3 +1,11 @@
+<?php 
+//check login
+if(empty($USER->username)){
+	header( "refresh: 0; url=../../login/index.php" );		//redirect to http://localhost/moodle/login/index.php
+	exit(0);
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
