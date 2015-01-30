0b9cebb16ac5e18d7bc0a19d2487fe98264606a2
Supanut Dokmaithong <boomhack320@gmail.com>
2015-01-30 21:31:24 +0700

    -class answer service ตอนนี้การทำงานเหมือนกัน log service มาก ๆ แค่เปลี่ยนจาก code เป็น answer ดังนั้นมีแนวโน้มที่จะเขียนใหม่ เป็น class เดียวกัน -แต่ก็ยังไม่อยากเปลี่ยนเพราะ อนาคตอาจปรับแต่ class เพิ่มเติม เพราะแต่ละส่วนทำงานต่างการน่าจะมี method ที่แตกต่างกันไป

--- source/controller_service.php
+++ source/controller_service.php
@@ -50,12 +50,11 @@ if($mode == "log")
 		echo "Error: type was empty";
 		exit(0);
 	}
-	else if(($type != "while_exp") && ($type != "after_exp"))
+	else if(($type != 1) && ($type != 2))
 	{
-		echo "Error: type is wrong. Only type is while_exp or after_exp";
+		echo "Error: type is wrong. Only type is 1(while_exp) or 2(after_exp)";
 		exit(0);
 	}
-
 	$sid = $USER->username;
 
 	//echo $sid.",".$code.",".$unit.",".$article.",".$type;
@@ -72,7 +71,61 @@ if($mode == "log")
 
 if($mode == "answer")
 {
-	echo "Hello answer";
+	$answer = $_REQUEST["answer"];
+	if(empty($answer))
+	{
+		echo "Error: answer was empty";
+		exit(0);
+	}
+
+	$unit = $_REQUEST["unit"];
+	if(empty($unit))
+	{
+		echo "Error: unit was empty";
+		exit(0);
+	}
+	else if(!is_numeric($unit))
+	{
+		echo "Error: unit is wrong. Unit only is integer.";
+		exit(0);
+	}	
+
+	$article = $_REQUEST["article"];
+	if(empty($article))
+	{
+		echo "Error: article was empty";
+		exit(0);
+	}
+	else if(!is_numeric($article))
+	{
+		echo "Error: article is wrong. Article only is integer.";
+		exit(0);
+	}		
+
+	$type = $_REQUEST["type"];
+	if(empty($type))
+	{
+		echo "Error: type was empty";
+		exit(0);
+	}
+	else if(($type != 1) && ($type != 2))
+	{
+		echo "Error: type is wrong. Only type is 1(while_exp) or 2(after_exp)";
+		exit(0);
+	}
+
+	$sid = $USER->username;
+
+	//echo $sid.",".$code.",".$unit.",".$article.",".$type;
+	include("./source/php/model_connection.php");				//run on service.php outsite source folder
+	include("./source/php/model_service_answer.php");
+
+	$cdb = new connectDB('localhost','root','1234','moodle');
+	$cdb->connect();
+	$con = $cdb->con;											//get con for send to logService class
+	//print_r($con);
+	$as = new answerService($con,$sid,$answer,$unit,$article,$type);
+	$as->pushAnswer();
 }
 
 
