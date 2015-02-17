7a60773c94002bbce60a71949c1102ae29814a92
Supanut Dokamithong <boomhack320@gmail.com>
2015-02-17 15:32:03 +0700

    สร้าง function callContent

---
+++ source/js/dashboard.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){	
+
+//start load index content
+	callContent("checking.php");
+//end load index content
+
+
+//start function zone
+function callContent(source)
+{
+	$.ajax({
+			url:"./source/content/"+source,
+			type: "POST",
+			data: "",
+			success:function(result)
+			{
+				$('#content').html(result);
+			}
+    	});
+}
+//end function zone
+});
