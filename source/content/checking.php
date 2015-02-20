2cf23fe157554dd3d575587620b7dfa376eba519
Supanut Dokamithong <boomhack320@gmail.com>
2015-02-20 15:29:29 +0700

    เลือกแสดง content เมื่อเข้าเว็บครั้งแรก -ถ้ายังไม่ได้ตั้งค่า UNIT โหลดหน้า setting.php -ถ้าตั้งค่า UNIT แล้ว จะโหลดหน้า assignment.php

--- source/content/checking.php
+++ source/content/checking.php
@@ -1,3 +1,32 @@
 <?php 
-include("./setting.php");
+include("../php/model_connection.php");
+include("../php/model_getJsonConnection.php");
+
+//get json connection
+$gjc = new getJsonConnection();
+$c = $gjc->getConnection('../connection.json');
+//get connect to db
+$cdb = new connectDB($c['host'],$c['user'],$c['pass'],$c['dbname']);
+$cdb->connect();
+$con = $cdb->con;
+
+$select = "select * from mdl_mysql_unit";
+if($result = mysqli_query($con,$select))
+{
+	$numrow = mysqli_num_rows($result);
+	if($numrow == 0)						//first using mysqlreport 
+	{
+		include("./setting.php");
+	}
+	else
+	{
+		include("./assignment.php");
+	}
+}
+else
+{
+	printf("Error: %s", mysqli_error($con));
+	exit();
+}
+
  ?>
